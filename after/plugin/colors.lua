d1830a3e75601d8c97a94cc3462daefb84d39408
paalss <paalss94@gmail.com>
2023-05-21 17:57:27 +0200

    feat: add color scheme rose pine

---
+++ after/plugin/colors.lua
@@ -0,0 +1,13 @@
+function ColorMyPencils(color)
+	-- set the color scheme
+	-- default color settes til rose-pine
+	color = color or "rose-pine"
+	vim.cmd.colorscheme(color)
+
+	-- set transparent background
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+end
+
+ColorMyPencils()
