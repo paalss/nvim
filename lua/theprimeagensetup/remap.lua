07ec3caf810ae5d15fb76fa53c159751cb7bd5bd
paalss <paalss94@gmail.com>
2023-05-22 22:56:38 +0200

    chore: remapping attempt

--- lua/theprimeagensetup/remap.lua
+++ lua/theprimeagensetup/remap.lua
@@ -3,3 +3,10 @@ vim.g.mapleader = " "
 -- når i normal mode "n", trykk leader-knappen for å kjøre command :Ex
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- Da kan du, i stedet for å kjøre `:Ex` for å dra til nvim file exploreren, trykke [space] p v. (raskt!)
+
+-- forsøk på remapping av knapper
+-- vim.keymap.set("i", "<sc-8>", "{", {noremap = true})
+-- vim.keymap.set("n", "<sc-8"", [[<cmd>lua print('ctrl+shift+8')<cr>]], {noremap = true})
+-- vim.api.nvim_set_keymap("<c-8>", "<s-y>", {noremap=true}) 
+
+
