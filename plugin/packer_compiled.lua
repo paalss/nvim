4c1750208343aa9ca42e0307722d3c39baf3bb56
paalss <paalss94@gmail.com>
2023-05-23 22:22:05 +0200

    feat: add harpoon and undotree packages

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -74,6 +74,11 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  harpoon = {
+    loaded = true,
+    path = "/home/paalss/.local/share/nvim/site/pack/packer/start/harpoon",
+    url = "https://github.com/theprimeagen/harpoon"
+  },
   ["nvim-treesitter"] = {
     loaded = true,
     path = "/home/paalss/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
@@ -99,6 +104,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/paalss/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
+  },
+  undotree = {
+    loaded = true,
+    path = "/home/paalss/.local/share/nvim/site/pack/packer/start/undotree",
+    url = "https://github.com/mbbill/undotree"
   }
 }
 
