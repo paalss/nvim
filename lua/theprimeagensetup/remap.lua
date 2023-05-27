d101fc0ee12816aa412dc2d81895aece9601d0c6
paalss <paalss94@gmail.com>
2023-05-27 17:57:37 +0200

    refactor: delete comment

--- lua/theprimeagensetup/remap.lua
+++ lua/theprimeagensetup/remap.lua
@@ -8,8 +8,3 @@ vim.keymap.set("i", "<S-j>", "<Esc>:m+<CR>")
 vim.keymap.set("i", "<S-k>", "<Esc>:m-2<CR>")
 -- Da kan du, i stedet for å kjøre `:Ex` for å dra til nvim file exploreren, trykke [space] p v. (raskt!)
 
--- forsøk på remapping av knapper
--- vim.keymap.set("i", "<sc-8>", "{", {noremap = true})
--- vim.keymap.set("n", "<sc-8"", [[<cmd>lua print('ctrl+shift+8')<cr>]], {noremap = true})
--- vim.api.nvim_set_keymap("<c-8>", "<s-y>", {noremap=true}) 
-
