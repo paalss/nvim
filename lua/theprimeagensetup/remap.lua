c8ae67d02f3832206ceaf66a2aa02e08a6bbcdb9
paalss <paalss94@gmail.com>
2023-05-28 21:08:59 +0200

    feat: add keymaps for {} and []

--- lua/theprimeagensetup/remap.lua
+++ lua/theprimeagensetup/remap.lua
@@ -8,3 +8,8 @@ vim.keymap.set("n", "<A-j>", ":m+<CR>") -- Alt j --> mode line up
 vim.keymap.set("i", "<A-j>", "<Esc>:m+<CR>")
 vim.keymap.set("i", "<A-k>", "<Esc>:m-2<CR>")
 
+
+vim.keymap.set("i", "<A-p>", "{}<Esc>i")
+vim.keymap.set("i", "<A-å>", "}")
+vim.keymap.set("i", "<A-ø>", "[]<Esc>i")
+vim.keymap.set("i", "<A-æ>", "]")
