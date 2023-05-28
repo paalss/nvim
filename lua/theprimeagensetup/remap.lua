5db5ed549eb90beee1f2f0241ab8217ecb3d68f6
paalss <paalss94@gmail.com>
2023-05-28 14:10:45 +0200

    fix: change shortcut shift+j/k to alt+j/k
    and update above comment

--- lua/theprimeagensetup/remap.lua
+++ lua/theprimeagensetup/remap.lua
@@ -1,10 +1,10 @@
 -- definere leader som space-knappe
 vim.g.mapleader = " "
--- når i normal mode "n", trykk leader-knappen for å kjøre command :Ex
+-- når i normal mode "n", trykk eg. [space] pv for å kjøre command :Ex
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<S-k>", ":m-2<CR>") -- Shift k --> move line down
-vim.keymap.set("n", "<S-j>", ":m+<CR>") -- Shift j --> mode line up
-vim.keymap.set("i", "<S-j>", "<Esc>:m+<CR>")
-vim.keymap.set("i", "<S-k>", "<Esc>:m-2<CR>")
+vim.keymap.set("n", "<A-k>", ":m-2<CR>") -- Alt k --> move line down
+vim.keymap.set("n", "<A-j>", ":m+<CR>") -- Alt j --> mode line up
+vim.keymap.set("i", "<A-j>", "<Esc>:m+<CR>")
+vim.keymap.set("i", "<A-k>", "<Esc>:m-2<CR>")
 -- Da kan du, i stedet for å kjøre `:Ex` for å dra til nvim file exploreren, trykke [space] p v. (raskt!)
 
