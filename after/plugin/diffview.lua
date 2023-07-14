e9be9a2e24948b432a736e4af4e7d0fd9d4d7043
paalss <paalss94@gmail.com>
2023-07-14 21:02:18 +0200

    feat: add shortcuts for diffview

---
+++ after/plugin/diffview.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "<leader>glo", ":DiffviewFileHistory<CR>")
+vim.keymap.set("n", "<leader>glgp", ":DiffviewFileHistory %<CR>")
+vim.keymap.set("n", "<leader>gd", ":DiffviewOpen<CR>")
+
