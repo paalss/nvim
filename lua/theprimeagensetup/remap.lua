88c6f90b0a555660a6a90f1e675f2ea57c9d34da
paalss <paalss94@gmail.com>
2023-07-16 21:55:23 +0200

    style: add spaces and comment

--- lua/theprimeagensetup/remap.lua
+++ lua/theprimeagensetup/remap.lua
@@ -18,8 +18,11 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
+
+-- page up/down
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
 vim.keymap.set("n", "n", "nzzzv")  -- keep search terms in the middle
 vim.keymap.set("n", "N", "Nzzzv")
 
