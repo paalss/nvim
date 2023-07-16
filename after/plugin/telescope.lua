366c36de5678d32eb25b07aa0a5039742d81346d
paalss <paalss94@gmail.com>
2023-07-16 15:42:42 +0200

    fix: insert empty search keyword
    to prevent unintended search-words from file

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,5 +2,5 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string();
+    builtin.grep_string({search=""});
 end)
