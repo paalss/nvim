afd2beedec6d1c54886f0a6c4839a12acadd7651
paalss <paalss94@gmail.com>
2023-07-19 22:03:07 +0200

    style: format packer

--- lua/theprimeagensetup/packer.lua
+++ lua/theprimeagensetup/packer.lua
@@ -5,9 +5,8 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
-
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         -- or                            , branch = '0.1.x',
