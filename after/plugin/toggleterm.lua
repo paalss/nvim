e7ea62fb65677bfbb2fe463f111616f80692b431
paalss <paalss94@gmail.com>
2023-07-19 17:54:50 +0200

    feat: add hotkey for toggling terminal

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -3,27 +3,11 @@ if not status_ok then
     return
 end
 --
--- 248 = øåæ
---
 -- husk at, om du ikke får til å lukke terminal med samme command som du åpnet med,
 -- så må du exite og åpne denne fila på nytt 
 -- er ofte ikke nok å :w og :so
 
 toggleterm.setup({
-    open_mapping = [[<F7>]], -- ok
-    -- open_mapping = "<F7>", -- ok
-    -- open_mapping = "<A-l>", -- ok
-    -- open_mapping = "<C-l>", -- ok
-    -- open_mapping = "<A-ø>", -- ok
-    -- open_mapping = "<A-Char-248>", -- ok
-
-    -- open_mapping = "<C-ø>", -- fail
-    -- open_mapping = "<C-Char-248>", -- fail
-
-    -- open_mapping = "<C-[>", -- fail, men nesten! det er for bokstaven Å! Og så vil denne få ESC til å åpne terminal også.... :/
-    -- open_mapping = "<C-o/>", -- fail
-    -- open_mapping = [[<C-\>]], -- fail. finner ikke hvilken tast
-    -- open_mapping = "<C-]>", -- fail. finner ikke hvilken tast som tilsvarer ]
-
+    open_mapping = "<F7>", -- ok
     direction = "float"
 })
--- hotkeys-for-windows.ahk
+++ hotkeys-for-windows.ahk
@@ -5,10 +5,22 @@
 ; ^ ctrl
 ; + Shift
 
-::ftw::Free the whales ; ved å skrive ftw og mellomrom, blir autocorrect til Free the whales.
+::ftw::Free the whales ; ved å skrive ftw og mellomrom/tab, blir autocorrect til Free the whales.
  
 ^+8::SendText "{" ; Ctrl Shift 8
 
 ^+9::SendText "}" ; Ctrl Shift 9
+
+; hvordan kjøre script fra terminalen??
+
+; Toggle terminal
+
+^ø::Send "{F7}" ; (toggleterm.lua uses this button) 
+
+; kopiere linjer (kjøre yyp og yyP), må muligens fjerne 'move line up/down' fra remap.lua
+
+; navigate barbar med ctrl pgup/pgdown
+; 
+
 
 !Esc::ExitApp
