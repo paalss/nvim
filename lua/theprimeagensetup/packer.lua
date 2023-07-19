fcf38b333f4ba6812ed862e4a780c16622101ed1
paalss <paalss94@gmail.com>
2023-07-19 22:07:32 +0200

    feat: install autopairs
    automatically create closing braces when opening new ones

--- lua/theprimeagensetup/packer.lua
+++ lua/theprimeagensetup/packer.lua
@@ -8,6 +8,10 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
     use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
+    use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         -- or                            , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
