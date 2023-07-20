19298c3df3682da7e1c81f42c6d53aaa49512b72
paalss <paalss94@gmail.com>
2023-07-20 22:45:09 +0200

    style: format packer

--- lua/theprimeagensetup/packer.lua
+++ lua/theprimeagensetup/packer.lua
@@ -1,4 +1,3 @@
-
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
@@ -14,7 +13,7 @@ return require('packer').startup(function(use)
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         -- or                            , branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
     use 'sindrets/diffview.nvim'
     use {
@@ -24,7 +23,7 @@ return require('packer').startup(function(use)
     use {
         "nvim-neo-tree/neo-tree.nvim",
         branch = "v2.x",
-        requires = { 
+        requires = {
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
             "MunifTanjim/nui.nvim",
@@ -40,40 +39,45 @@ return require('packer').startup(function(use)
 
     -- These optional plugins should be loaded directly because of a bug in Packer lazy loading
     use('nvim-tree/nvim-web-devicons') -- OPTIONAL: for file icons
-    use('lewis6991/gitsigns.nvim') -- OPTIONAL: for git status
+    use('lewis6991/gitsigns.nvim')     -- OPTIONAL: for git status
     use('romgrk/barbar.nvim')
-    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('mbbill/undotree')
     use({
-	    "kdheepak/lazygit.nvim",
-	    -- optional for floating window border decoration
-	    requires = {
-		    "nvim-lua/plenary.nvim",
-	    },
+        "kdheepak/lazygit.nvim",
+        -- optional for floating window border decoration
+        requires = {
+            "nvim-lua/plenary.nvim",
+        },
     })
     use('tpope/vim-commentary')
-    use {"akinsho/toggleterm.nvim", tag = '*', config = function()
-	    require("toggleterm").setup()
-    end}
     use('tpope/vim-fugitive')
+    use { "akinsho/toggleterm.nvim", tag = '*', config = function()
+        require("toggleterm").setup()
+    end }
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = function() vim.fn["mkdp#util#install"]() end,
+    })
     use {
-	    'VonHeikemen/lsp-zero.nvim',
-	    branch = 'v2.x',
-	    requires = {
-		    -- LSP Support
-	 	    {'neovim/nvim-lspconfig'},             -- Required
-		    {                                      -- Optional
-		    'williamboman/mason.nvim',
-		    run = function()
-			    pcall(vim.cmd, 'MasonUpdate')
-		    end,
-	    },
-	    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {
+                -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-		-- Autocompletion
-		{'hrsh7th/nvim-cmp'},     -- Required
-		{'hrsh7th/cmp-nvim-lsp'}, -- Required
-		{'L3MON4D3/LuaSnip'},     -- Required
-	}
-}
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' }, -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' }, -- Required
+        }
+    }
 end)
