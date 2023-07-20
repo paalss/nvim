9ba64a4617cfe28df24f20a933bad2eb253e84e5
paalss <paalss94@gmail.com>
2023-07-20 18:14:38 +0200

    chore: test change

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
 require("theprimeagensetup")
 require("after.plugin.colors")
 ColorMyPencils()
+
+vim.fn.jobstart('timeout 0.1s bash autohotkey/start-ahk.sh')
 -- init.lua is like index.html. It's the file it looks for.
+
