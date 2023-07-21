c530fc2a6492577fc2b65adfed460325e7ebe62f
paalss <paalss94@gmail.com>
2023-07-21 17:45:33 +0200

    chore: configure lazygit to use git delta for diffs

--- after/plugin/lazygit.lua
+++ after/plugin/lazygit.lua
@@ -1,1 +1,3 @@
 vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>")
+vim.g.lazygit_use_custom_config_file_path = 1 -- 1 = enable custom file path
+vim.g.lazygit_config_file_path = '~/.config/nvim/lazygit/config.yml' -- set to config file in this repo's config.yml
