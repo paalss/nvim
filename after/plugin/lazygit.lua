4cd4c72653ed781b72247d83ebb1ae4d44bb53fa
paalss <paalss94@gmail.com>
2023-07-21 18:51:32 +0200

    chore: move shortcuts from diffview to lazygit

--- after/plugin/lazygit.lua
+++ after/plugin/lazygit.lua
@@ -1,3 +1,6 @@
 vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>")
 vim.g.lazygit_use_custom_config_file_path = 1 -- 1 = enable custom file path
 vim.g.lazygit_config_file_path = '~/.config/nvim/lazygit/config.yml' -- set to config file in this repo's config.yml
+
+vim.keymap.set("n", "<leader>glo", ":LazyGitFilter<CR>")
+vim.keymap.set("n", "<leader>glgp", ":LazyGitFilterCurrentFile<CR>")
