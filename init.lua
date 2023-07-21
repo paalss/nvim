5e1137d67794d23c4a2e0a2cb2a2f6e82679f6ee
paalss <paalss94@gmail.com>
2023-07-21 19:31:27 +0200

    fix: remove line referencing to unavailable folder

--- init.lua
+++ init.lua
@@ -1,6 +1,4 @@
 require("theprimeagensetup")
-require("after.plugin.colors")
-ColorMyPencils()
 
 -- vim.fn.jobstart('timeout 0.1s bash autohotkey/start-ahk.sh')
 -- init.lua is like index.html. It's the file it looks for.
