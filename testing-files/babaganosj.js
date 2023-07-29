04cf8763279469465e7b1c54de6f1061c78ef656
paalss <paalss94@gmail.com>
2023-07-29 19:13:16 +0200

    refactor: add testing files
    for playing around in and demonstrating colorcheme and testing features

---
+++ testing-files/babaganosj.js
@@ -0,0 +1,15 @@
+const { log } = require("console")
+
+function babaganosj() {
+    console.log('hallo på do folkens')
+    log('hello folkens')
+}
+
+babaganosj()
+
+const detteErEnLitenSpoek = () => {
+    // som blir ødelagt hvert forsøk
+}
+
+const bool = true
+let string = "heeeiååå"
