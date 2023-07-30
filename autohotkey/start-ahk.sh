58e5fe72f9025ce25d6ac2a66788775bae18f951
paalss <paalss94@gmail.com>
2023-07-30 17:23:10 +0200

    fix: make start-ahk.sh work when run from different directory than ~/.config/nvim

--- autohotkey/start-ahk.sh
+++ autohotkey/start-ahk.sh
@@ -1,1 +1,1 @@
-'/mnt/c/Program Files/AutoHotkey/v2/AutoHotkey.exe' autohotkey/hotkeys-for-windows.ahk
+'/mnt/c/Program Files/AutoHotkey/v2/AutoHotkey.exe' ~/.config/nvim/autohotkey/hotkeys-for-windows.ahk
