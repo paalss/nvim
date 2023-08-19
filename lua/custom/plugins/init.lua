9b05b83c84a29353b0a19b2e59365707e2689375
paalss <paalss94@gmail.com>
2023-08-19 17:27:56 +0200

    refactor(scope): move stuff around

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,37 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {{
+    "windwp/nvim-autopairs",
+    -- Optional dependency
+    dependencies = {'hrsh7th/nvim-cmp'},
+    config = function()
+        require("nvim-autopairs").setup {}
+        -- If you want to automatically add `(` after selecting a function or method
+        local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+        local cmp = require('cmp')
+        cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end
+}, ('mbbill/undotree'), ({
+    "kdheepak/lazygit.nvim",
+    -- optional for floating window border decoration
+    dependencies = {"nvim-lua/plenary.nvim"}
+}), {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = { -- LSP Support
+    {'neovim/nvim-lspconfig'}, -- Required
+    {
+        -- Optional
+        'williamboman/mason.nvim',
+        build = function()
+            pcall(vim.cmd, 'MasonUpdate')
+        end
+    }, {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'}, -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'} -- Required
+    }
+}}
+
--- lua/custom/plugins/remap.lua
+++ lua/custom/plugins/remap.lua
@@ -1,14 +1,4 @@
-vim.g.mapleader = " "
--- returnere til nvim file explorer
-
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- space pv
-
--- move line
--- overlatt til AHK
--- vim.keymap.set("n", "<A-k>", ":m-2<CR>")  -- Alt k --> move line down
--- vim.keymap.set("n", "<A-j>", ":m+<CR>")  -- Alt j --> mode line up
--- vim.keymap.set("i", "<A-j>", "<Esc>:m+<CR>")
--- vim.keymap.set("i", "<A-k>", "<Esc>:m-2<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
