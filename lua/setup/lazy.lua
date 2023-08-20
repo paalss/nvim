997038de7ad82c0f8cd2987b45b356e8c0204db2
paalss <paalss94@gmail.com>
2023-08-20 15:00:49 +0200

    style(lazy.lua): format file

--- lua/setup/lazy.lua
+++ lua/setup/lazy.lua
@@ -1,15 +1,15 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system(
-        {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
-         lazypath})
+        { "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git",
+            "--branch=stable", -- latest stable release
+            lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = " "
 
 local plugins = {
-
     {
         "windwp/nvim-autopairs",
         config = function()
@@ -20,7 +20,7 @@ local plugins = {
         'nvim-telescope/telescope.nvim',
         version = '0.1.1',
         -- or                            , branch = '0.1.x',
-        dependencies = {{'nvim-lua/plenary.nvim'}}
+        dependencies = { { 'nvim-lua/plenary.nvim' } }
     },
     {
         "kylechui/nvim-surround",
@@ -46,13 +46,14 @@ local plugins = {
     {
         "nvim-neo-tree/neo-tree.nvim",
         branch = "v2.x",
-        dependencies = {"nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-                        "MunifTanjim/nui.nvim"}
+        dependencies = { "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim" }
     },
     'martinsione/darkplus.nvim',
     -- These optional plugins should be loaded directly because of a bug in Packer lazy loading
     'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-    'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+    'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
     'romgrk/barbar.nvim',
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
@@ -60,7 +61,7 @@ local plugins = {
     {
         "kdheepak/lazygit.nvim",
         -- optional for floating window border decoration
-        dependencies = {"nvim-lua/plenary.nvim"}
+        dependencies = { "nvim-lua/plenary.nvim" }
     },
     'tpope/vim-commentary',
     {
@@ -80,19 +81,19 @@ local plugins = {
     {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
-        dependencies = { -- LSP Support
-        {'neovim/nvim-lspconfig'}, -- Required
-        {
-            -- Optional
-            'williamboman/mason.nvim',
-            build = function()
-                pcall(vim.cmd, 'MasonUpdate')
-            end
-        }, {'williamboman/mason-lspconfig.nvim'}, -- Optional
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'}, -- Required
-        {'hrsh7th/cmp-nvim-lsp'}, -- Required
-        {'L3MON4D3/LuaSnip'} -- Required
+        dependencies = {             -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {
+                -- Optional
+                'williamboman/mason.nvim',
+                build = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end
+            }, { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },                 -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },             -- Required
+            { 'L3MON4D3/LuaSnip' }                  -- Required
         }
     }
 }
