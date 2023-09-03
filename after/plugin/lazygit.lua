b6d5b73b39742b9a18af7cefcc42e2944f71375b
paalss <paalss94@gmail.com>
2023-09-03 14:04:22 +0200

    refactor: update comment

--- after/plugin/lazygit.lua
+++ after/plugin/lazygit.lua
@@ -1,6 +1,6 @@
 vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>")
-vim.g.lazygit_use_custom_config_file_path = 1 -- 1 = enable custom file path
-vim.g.lazygit_config_file_path = '~/.config/lazygit/config.yml' -- set to config file in this repo's config.yml
+vim.g.lazygit_use_custom_config_file_path = 1                   -- 1 = enable custom file path
+vim.g.lazygit_config_file_path = '~/.config/lazygit/config.yml' -- where config file is storend config file
 
 vim.keymap.set("n", "<leader>glo", ":LazyGitFilter<CR>")
 vim.keymap.set("n", "<leader>glgp", ":LazyGitFilterCurrentFile<CR>")
