dc017b0388b71a5e7116766e38f1fd352d836f97
paalss <paalss94@gmail.com>
2023-09-10 12:28:17 +0200

    feat(lsp): shortcut for aborting lsp suggestion

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -17,6 +17,7 @@ cmp.setup {
         ['<C-p>'] = cmp.mapping.select_prev_item(),
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-s>'] = cmp.mapping.abort(),
         ['<C-Space>'] = cmp.mapping.complete {},
         ['<CR>'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
