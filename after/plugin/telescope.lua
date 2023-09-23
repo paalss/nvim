eb13c9bfd4169136cc2ce28a9ef039f910affa00
paalss <paalss94@gmail.com>
2023-09-23 11:59:51 +0200

    feat: new keyboard shortcuts for telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,15 @@
 local builtin = require('telescope.builtin')
 require('telescope').load_extension("adjacent")
 vim.keymap.set('n', '<leader>cc', "<cmd>Telescope adjacent<CR>", { noremap = true, silent = false })
+
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+
+vim.keymap.set('n', '<A-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
+
+vim.keymap.set('n', '<leader>pp', function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
