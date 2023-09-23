6a3f354c583794a6cadfe3e3573c80f40e0c9319
paalss <paalss94@gmail.com>
2023-09-23 12:09:30 +0200

    fix: harpoon shortcuts

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -6,5 +6,5 @@ vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<A-1>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<A-2>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<A-3>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<A-4>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<A-3>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<A-4>", function() ui.nav_file(4) end)
