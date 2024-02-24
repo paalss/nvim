493647c5a8153359f782d68e710664f5f6054bb8
paalss <paalss94@gmail.com>
2024-02-24 23:53:06 +0100

    feat: add hard coded 'macros'

--- after/plugin/which-key.lua
+++ after/plugin/which-key.lua
@@ -8,3 +8,19 @@ wk.register({
     "Git blame"
   },
 }, { prefix = "<leader>" })
+
+wk.register({
+  name = "hard coded macros",
+  e = {
+    name = "className={} toggle `${}`",
+    e = {"f{a`${<esc>f}i}`<esc>B", "{classes.___} -> {`${classes.___} `}" },
+    d = {"f`xxxf}xxB", "{`${classes.___}`} -> {classes.___}" },
+  },
+  a = { "ggVG", "mark all" },
+  ya = { "ggVGy", "yank all" },
+  pa = { "ggVGp", "paste all" },
+  s = {
+    name = "case toggle",
+    s = {"f_x~f_x~", "snake_case -> camelCase" },
+  }
+}, { prefix = "-"})
