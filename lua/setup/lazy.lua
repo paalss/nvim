d5f5b7c93586a14daec358e193a2ae7d5b1b1661
paalss <paalss94@gmail.com>
2024-02-28 20:53:31 +0100

    feat: setup preview commands for norm and @
    :Norm -> preview :norm
    :At -> preview @

--- lua/setup/lazy.lua
+++ lua/setup/lazy.lua
@@ -30,6 +30,25 @@ local plugins = {
     opts = {},
   },
   {
+    'smjonas/live-command.nvim',
+    pin = true,
+    config = function()
+      require("live-command").setup {
+        commands = {
+          Norm = { cmd = "norm" },
+          At = {
+            cmd = "norm",
+            -- This will transform ":5At a" into ":norm 5@a"
+            args = function(opts)
+              return (opts.count == -1 and "" or opts.count) .. "@" .. opts.args
+            end,
+            range = "",
+          },
+        }
+      }
+    end
+  },
+  {
     "nvim-neorg/neorg",
     build = ":Neorg sync-parsers",
     -- tag = "*",
