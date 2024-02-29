ac1fc958887c827c211da8ae9857b7f8e25a8937
Pål Syvertsen Stakvik <paalss94@gmail.com>
2024-02-29 21:01:16 +0100

    chore: install legendary
    Menu for all commands yay!

--- lua/setup/lazy.lua
+++ lua/setup/lazy.lua
@@ -11,6 +11,16 @@ vim.g.mapleader = " "
 
 local plugins = {
   {
+  'mrjones2014/legendary.nvim',
+  version = 'v2.1.0',
+  -- since legendary.nvim handles all your keymaps/commands,
+  -- its recommended to load legendary.nvim before other plugins
+  priority = 10000,
+  lazy = false,
+  -- sqlite is only needed if you want to use frecency sorting
+  -- dependencies = { 'kkharji/sqlite.lua' }
+  },
+  {
     "folke/which-key.nvim",
     event = "VeryLazy",
     init = function()
