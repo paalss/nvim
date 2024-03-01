dc9507bd5d8c2198353c834eab47d97d186f72fb
paalss <paalss94@gmail.com>
2024-03-01 23:56:42 +0100

    style(lazy): format

--- lua/setup/lazy.lua
+++ lua/setup/lazy.lua
@@ -11,14 +11,14 @@ vim.g.mapleader = " "
 
 local plugins = {
   {
-  'mrjones2014/legendary.nvim',
-  version = 'v2.1.0',
-  -- since legendary.nvim handles all your keymaps/commands,
-  -- its recommended to load legendary.nvim before other plugins
-  priority = 10000,
-  lazy = false,
-  -- sqlite is only needed if you want to use frecency sorting
-  -- dependencies = { 'kkharji/sqlite.lua' }
+    'mrjones2014/legendary.nvim',
+    version = 'v2.1.0',
+    -- since legendary.nvim handles all your keymaps/commands,
+    -- its recommended to load legendary.nvim before other plugins
+    priority = 10000,
+    lazy = false,
+    -- sqlite is only needed if you want to use frecency sorting
+    -- dependencies = { 'kkharji/sqlite.lua' }
   },
   {
     "folke/which-key.nvim",
