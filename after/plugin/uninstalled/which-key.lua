4dcae3c4c0805d7623432553fd8143b6995869f4
paalss <paalss94@gmail.com>
2024-03-02 21:21:44 +0100

    fix: uninstall which key

---
+++ after/plugin/uninstalled/which-key.lua
@@ -0,0 +1,29 @@
+-- local wk = require("which-key")
+
+-- wk.register({
+--   a = {
+--     "Add to harpoon"
+--   },
+--   hd = {
+--     "Diff this"
+--   },
+--   tb = {
+--     "Git blame"
+--   },
+-- }, { prefix = "<leader>" })
+
+-- wk.register({
+--   name = "hard coded macros",
+--   e = {
+--     name = "className={} toggle `${}`",
+--     e = { "f{a`${<esc>f}i}`<esc>B", "{classes.___} -> {`${classes.___} `}" },
+--     d = { "f`xxxf}xxB", "{`${classes.___}`} -> {classes.___}" },
+--   },
+--   a = { "ggVG", "mark all" },
+--   ya = { "ggVGy", "yank all" },
+--   pa = { "ggVGp", "paste all" },
+--   s = {
+--     name = "case toggle",
+--     s = { "f_x~", "snake_case -> camelCase" },
+--   }
+-- }, { prefix = "-" })
--- lua/setup/lazy.lua
+++ lua/setup/lazy.lua
@@ -25,19 +25,6 @@ local plugins = {
     opts = {},
   },
   {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-    end,
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
-  },
-  {
     "folke/trouble.nvim",
     pin = true,
     dependencies = { "nvim-tree/nvim-web-devicons" },
