6f62e866da94c653cf772959cc642e56d69536e2
paalss <paalss94@gmail.com>
2024-03-02 17:42:01 +0100

    feat(legendary): add more keymaps

--- after/plugin/legendary.lua
+++ after/plugin/legendary.lua
@@ -6,3 +6,7 @@ require('legendary').setup({
 })
 
 map('n', '<leader>leg', '<Cmd>Legendary<CR>', {})
+map('n', '<leader>lek', '<Cmd>Legendary keymaps<CR>', {})
+map('n', '<leader>lec', '<Cmd>Legendary commands<CR>', {})
+map('n', '<leader>lef', '<Cmd>Legendary functions<CR>', {})
+map('n', '<leader>lea', '<Cmd>Legendary autocmds<CR>', {})
