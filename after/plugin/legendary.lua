269967fa5e44bab3bb332ce497ee9af0d905e3f3
paalss <paalss94@gmail.com>
2024-03-02 20:43:07 +0100

    refactor(legendary): stuff

--- after/plugin/legendary.lua
+++ after/plugin/legendary.lua
@@ -1,6 +1,7 @@
 local map = vim.api.nvim_set_keymap
+local legendary = require('legendary')
 
-require('legendary').setup({
+legendary.setup({
   lazy_nvim = { auto_register = true },
   keymaps = {}
 })
