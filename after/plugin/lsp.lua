8b3b201e44b8ac2be54df1476f2c5793c9de2d52
paalss <paalss94@gmail.com>
2024-03-02 22:35:10 +0100

    fix(lsp): scroll down docs with u

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -27,7 +27,7 @@ cmp.setup {
         ['<C-n>'] = cmp.mapping.select_next_item(),
         ['<C-p>'] = cmp.mapping.select_prev_item(),
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-u>'] = cmp.mapping.scroll_docs(4),
         ['<C-s>'] = cmp.mapping.abort(),
         ['<C-Space>'] = cmp.mapping.complete {},
         ['<CR>'] = cmp.mapping.confirm {
