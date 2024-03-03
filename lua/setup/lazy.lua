15b6cf9cb823254fd584418cfb236f741ebd7318
paalss <paalss94@gmail.com>
2024-03-03 15:33:38 +0100

    fix: uninstall legendary

--- lua/setup/lazy.lua
+++ lua/setup/lazy.lua
@@ -11,16 +11,6 @@ vim.g.mapleader = " "
 
 local plugins = {
   {
-    'mrjones2014/legendary.nvim',
-    version = 'v2.1.0',
-    -- since legendary.nvim handles all your keymaps/commands,
-    -- its recommended to load legendary.nvim before other plugins
-    priority = 10000,
-    lazy = false,
-    -- sqlite is only needed if you want to use frecency sorting
-    -- dependencies = { 'kkharji/sqlite.lua' }
-  },
-  {
     'stevearc/dressing.nvim',
     opts = {},
   },
