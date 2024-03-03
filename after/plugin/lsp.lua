8586917fdf94c6eee9ad9ca110af97f228e6df00
paalss <paalss94@gmail.com>
2024-03-03 15:38:07 +0100

    fix(lsp): add descriptions to keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,10 +3,10 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 lsp.ensure_installed({
-    'tsserver',
-    'eslint',
-    'lua_ls',
-    'rust_analyzer',
+  'tsserver',
+  'eslint',
+  'lua_ls',
+  'rust_analyzer',
 })
 
 
@@ -18,64 +18,103 @@ require('luasnip.loaders.from_vscode').lazy_load()
 luasnip.config.setup {}
 
 cmp.setup {
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end
+  },
+  mapping = cmp.mapping.preset.insert {
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-u>'] = cmp.mapping.scroll_docs(4),
+    ['<C-s>'] = cmp.mapping.abort(),
+    ['<C-Space>'] = cmp.mapping.complete {},
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = false
     },
-    mapping = cmp.mapping.preset.insert {
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-u>'] = cmp.mapping.scroll_docs(4),
-        ['<C-s>'] = cmp.mapping.abort(),
-        ['<C-Space>'] = cmp.mapping.complete {},
-        ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = false
-        },
-        -- navigate to next, text previewed in file
-        -- Enter will create full snippet
-        -- Any other than Tab or Enter will leave preview as is
-        ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_locally_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end, {'i', 's'}),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.locally_jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, {'i', 's'})
-    },
-    sources = { {
-        name = 'nvim_lsp'
-    }, {
-        name = 'luasnip'
-    } }
+    -- navigate to next, text previewed in file
+    -- Enter will create full snippet
+    -- Any other than Tab or Enter will leave preview as is
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' })
+  },
+  sources = { {
+    name = 'nvim_lsp'
+  }, {
+    name = 'luasnip'
+  } }
 }
 
 lsp.on_attach(function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
+  local opts = { buffer = bufnr, remap = false }
 
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("n", "<leader>help", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, {
+    desc = "Go to definition",
+    buffer = bufnr,
+    remap = false
+  })
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, {
+    desc = "Show definition",
+    buffer = bufnr,
+    remap = false
+  })
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end,
+    {
+      desc = "open diagnostic",
+      buffer = bufnr,
+      remap = false
+    })
+
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end,
+    {
+      desc = "code action",
+      buffer = bufnr,
+      remap = false
+    })
+
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end,
+    {
+      desc = "code references",
+      buffer = bufnr,
+      remap = false
+    })
+
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end,
+    {
+      desc = "rename",
+      buffer = bufnr,
+      remap = false
+    })
+
+  vim.keymap.set("n", "<leader>help", function() vim.lsp.buf.signature_help() end,
+    {
+      desc = "help",
+      buffer = bufnr,
+      remap = false
+    })
 end)
 
 lsp.setup()
