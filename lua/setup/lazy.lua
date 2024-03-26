305e848b3e15e4b7d34e9066240ac9f695926bc3
paalss <paalss94@gmail.com>
2024-03-26 14:35:38 +0100

    feat(intro): add custom header

--- lua/setup/lazy.lua
+++ lua/setup/lazy.lua
@@ -11,14 +11,23 @@ vim.g.mapleader = " "
 
 local plugins = {
 	{
-		'nvimdev/dashboard-nvim',
-		event = 'VimEnter',
+		'goolord/alpha-nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
 		config = function()
-			require('dashboard').setup {
-				-- config
-			}
-		end,
-		dependencies = { { 'nvim-tree/nvim-web-devicons' } }
+      local alpha = require'alpha'
+      local dashboard = require'alpha.themes.dashboard'
+      dashboard.section.header.val = {
+             [[                               __                ]],
+             [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+             [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+             [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+             [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+             [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+             [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+             [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+      }
+      alpha.setup(dashboard.config)
+		end
 	},
 	{
 		'alvan/vim-closetag',
