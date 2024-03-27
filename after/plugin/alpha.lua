30e08d927d7389cc0e4695edc602202958c3a55b
paalss <paalss94@gmail.com>
2024-03-27 21:46:17 +0100

    feat(intro): list basic keybindings

--- after/plugin/alpha.lua
+++ after/plugin/alpha.lua
@@ -1,17 +1,45 @@
 local alpha = require 'alpha'
-local dashboard = require 'alpha.themes.dashboard'
-dashboard.section.header.val = {
-  [[  /$$$$$$   /$$               /$$                  /$$              ]],
-  [[ /$$__  $$ | $$              | $$                 |__/              ]],
-  [[| $$  \__//$$$$$$    /$$$$$$ | $$   /$$ /$$    /$$ /$$ /$$$$$$/$$$$ ]],
-  [[|  $$$$$$|_  $$_/   |____  $$| $$  /$$/|  $$  /$$/| $$| $$_  $$_  $$]],
-  [[ \____  $$ | $$      /$$$$$$$| $$$$$$/  \  $$/$$/ | $$| $$ \ $$ \ $$]],
-  [[ /$$  \ $$ | $$ /$$ /$$__  $$| $$_  $$   \  $$$/  | $$| $$ | $$ | $$]],
-  [[|  $$$$$$/ |  $$$$/|  $$$$$$$| $$ \  $$   \  $/   | $$| $$ | $$ | $$]],
-  [[ \______/   \___/   \_______/|__/  \__/    \_/    |__/|__/ |__/ |__/]],
+-- local dashboard = require 'alpha.themes.dashboard'
+
+local head = {
+  type = "text",
+  val = {
+    [[  /$$$$$$   /$$               /$$                  /$$              ]],
+    [[ /$$__  $$ | $$              | $$                 |__/              ]],
+    [[| $$  \__//$$$$$$    /$$$$$$ | $$   /$$ /$$    /$$ /$$ /$$$$$$/$$$$ ]],
+    [[|  $$$$$$|_  $$_/   |____  $$| $$  /$$/|  $$  /$$/| $$| $$_  $$_  $$]],
+    [[ \____  $$ | $$      /$$$$$$$| $$$$$$/  \  $$/$$/ | $$| $$ \ $$ \ $$]],
+    [[ /$$  \ $$ | $$ /$$ /$$__  $$| $$_  $$   \  $$$/  | $$| $$ | $$ | $$]],
+    [[|  $$$$$$/ |  $$$$/|  $$$$$$$| $$ \  $$   \  $/   | $$| $$ | $$ | $$]],
+    [[ \______/   \___/   \_______/|__/  \__/    \_/    |__/|__/ |__/ |__/]],
+  },
+  opts = {
+    position = "center",
+    hl = "Type",
+  },
 }
-dashboard.section.buttons.val = {
-  -- dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-  dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
+
+
+local block1 = {
+  type = "text",
+  val = {
+    [[CTRL I           Search keybindings]],
+    [[CTRL M           Open terminal (tmux pane)]],
+    [[CTRL X, M        Maximize pane]],
+  },
+  opts = {
+    position = "center",
+    hl = "Type",
+  },
 }
-alpha.setup(dashboard.config)
+
+
+local opts = {
+  layout = {
+    { type = "padding", val = 2 },
+    head,
+    { type = "padding", val = 2 },
+    block1,
+  }
+}
+alpha.setup(opts)
