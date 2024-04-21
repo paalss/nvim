44ae8ed0a84607df39001199df0b1b7c04fa8764
paalss <paalss94@gmail.com>
2024-04-21 21:16:21 +0200

    refactor(lazy): change vimscript to lua

--- lua/setup/lazy.lua
+++ lua/setup/lazy.lua
@@ -8,20 +8,18 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 
-vim.cmd [[
-let g:clipboard = {
-  \   'name': 'WslClipboard',
-  \   'copy': {
-    \      '+': 'clip.exe',
-    \      '*': 'clip.exe',
-    \    },
-    \   'paste': {
-      \      '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-      \      '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-      \   },
-      \   'cache_enabled': 0,
-      \ }
-]]
+vim.g.clipboard = {
+  name = 'WslClipboard',
+  copy = {
+    ['+'] = 'clip.exe',
+    ['*'] = 'clip.exe',
+  },
+  paste = {
+    ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = 0,
+}
 
 vim.g.mapleader = " "
 
