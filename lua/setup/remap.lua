ae5f40083acec67c44bdbea8cd25e81d20d63f59
paalss <paalss94@gmail.com>
2024-04-21 17:18:08 +0200

    chore: remove unnecessary +y keymaps and the like

--- lua/setup/remap.lua
+++ lua/setup/remap.lua
@@ -51,9 +51,10 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll page up" })
 
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
+-- not necessary as I'm using unnamedplus
+-- vim.keymap.set("n", "<leader>y", "\"+y")
+-- vim.keymap.set("v", "<leader>y", "\"+y")
+-- vim.keymap.set("n", "<leader>Y", "\"+Y")
 
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
