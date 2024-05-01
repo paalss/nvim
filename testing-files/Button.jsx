407f6462b5f8a122caeca6e896a4dc7acae1f467
paalss <paalss94@gmail.com>
2024-05-01 15:47:19 +0200

    chore: add jsx testing file

---
+++ testing-files/Button.jsx
@@ -0,0 +1,18 @@
+
+const a = 1;
+const b = 2
+// const c = 3;
+// const d = 4
+
+
+
+
+
+export const Button = ({ children }) => {
+  return (
+    <button>
+      <span>mmmmmmmmmmmmmm</span>
+      {children}
+    </button>
+  )
+}
