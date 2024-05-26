cf5cdf6f8edfc570461104515c49af745bd5667e
paalss <paalss94@gmail.com>
2024-05-26 20:11:48 +0200

    fix(fugitive): correct text description
    make it more searchable in telescope find keymaps

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,4 +1,4 @@
 vim.keymap.set("n", "<leader>gt", ":Git<CR>", { desc = "Open Git – Fugitive" })
 vim.keymap.set("n", "<leader>gf", ":Gwrite<CR>", { desc = "Save and stage/add buffer-file to git" })
 vim.keymap.set("n", "<leader>ga", ":G add -A<CR>", { desc = "Stage/add all files to git" })
-vim.keymap.set("n", "<leader>gc", ":G commit<CR>", { desc = "Commit" })
+vim.keymap.set("n", "<leader>gc", ":G commit<CR>", { desc = "Git commit" })
