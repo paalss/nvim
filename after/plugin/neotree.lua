e9737a2c387214ac3dabe26b08ff12826bda1590
paalss <paalss94@gmail.com>
2024-06-16 14:30:22 +0200

    refactor(lazy): install file system tree & keymaps, but disable it
    install it just for ease of access if I happened to want it. Maybe
    redundant as telescope, fugitive and lazygit also are installed.
    
    but a file tree is nice to have
    
    hmmmmmmmmmmmmmmm

---
+++ after/plugin/neotree.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "<leader>ee", "<cmd>Neotree toggle<cr>", { desc = "Toggle file explorer" })
+vim.keymap.set("n", "<leader>e3", "<cmd>Neotree close<cr>", { desc = "Close Neotree" })
+vim.keymap.set("n", "<leader>gss", "<cmd>Neotree git_status<cr>", { desc = "Show uncommited changes" })
--- lua/setup/lazy.lua
+++ lua/setup/lazy.lua
@@ -29,6 +29,17 @@ local plugins = {
   },
   { 'sbdchd/neoformat' },
   {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    enabled = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  },
+  {
     'homogulosus/vim-diff'
   },
   {
