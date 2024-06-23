f595cd35247107e2bbfad390d10ee9d28a03e0c8
paalss <paalss94@gmail.com>
2024-06-23 18:51:49 +0200

    feat(lazy): install file explorer neo tree

--- lua/setup/lazy.lua
+++ lua/setup/lazy.lua
@@ -31,7 +31,7 @@ local plugins = {
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
-    enabled = false,
+    enabled = true,
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
