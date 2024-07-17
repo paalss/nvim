be468a2e1922dc9659a45b60e11ff6db3074b1a6
paalss <paalss94@gmail.com>
2024-07-17 22:39:40 +0200

    feat(diffview): add keybindings
    for opening and closing Diffview

--- after/plugin/diffview.lua
+++ after/plugin/diffview.lua
@@ -1,4 +1,5 @@
 -- vim.keymap.set("n", "<leader>glo", ":DiffviewFileHistory<CR>")
 -- vim.keymap.set("n", "<leader>glgp", ":DiffviewFileHistory %<CR>")
--- vim.keymap.set("n", "<leader>gd", ":DiffviewOpen<CR>")
 
+vim.keymap.set("n", "<leader>gg", ":DiffviewOpen<CR>", { desc = "Open git status (compate current index)" })
+vim.keymap.set("n", "<leader>gh", ":DiffviewFocusFiles<CR><C-w>l:DiffviewClose<CR>", { desc = "Close Diffview" })
