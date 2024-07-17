9d6d85efd82f6a91295d559e6a76a7d078e880d4
paalss <paalss94@gmail.com>
2024-07-17 22:57:16 +0200

    feat(diffview): keymap for current-file-git-log

--- after/plugin/diffview.lua
+++ after/plugin/diffview.lua
@@ -1,5 +1,5 @@
 vim.keymap.set("n", "<leader>glo", ":DiffviewFileHistory<CR>", { desc = "Show git history" })
--- vim.keymap.set("n", "<leader>glgp", ":DiffviewFileHistory %<CR>")
+vim.keymap.set("n", "<leader>glgp", ":DiffviewFileHistory %<CR>", { desc = "Show git history for current file" })
 
 vim.keymap.set("n", "<leader>gg", ":DiffviewOpen<CR>", { desc = "Open git status (compate current index)" })
 vim.keymap.set("n", "<leader>gh", ":DiffviewFocusFiles<CR><C-w>l:DiffviewClose<CR>", { desc = "Close Diffview" })
