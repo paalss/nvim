b50dcb5b3a24e4a1e34c514fef1df3e5dbaae482
paalss <paalss94@gmail.com>
2024-07-17 23:47:28 +0200

    feat(diffview): keymap for comparing with main/master

--- after/plugin/diffview.lua
+++ after/plugin/diffview.lua
@@ -1,5 +1,11 @@
+-- Git log
 vim.keymap.set("n", "<leader>glo", ":DiffviewFileHistory<CR>", { desc = "Show git history" })
 vim.keymap.set("n", "<leader>glgp", ":DiffviewFileHistory %<CR>", { desc = "Show git history for current file" })
 
+-- Git status and close
 vim.keymap.set("n", "<leader>gg", ":DiffviewOpen<CR>", { desc = "Open git status (compate current index)" })
 vim.keymap.set("n", "<leader>gh", ":DiffviewFocusFiles<CR><C-w>l:DiffviewClose<CR>", { desc = "Close Diffview" })
+
+-- Compare working index with branch
+vim.keymap.set("n", "<leader>gy", ":DiffviewOpen origin/master<CR>", { desc = "Compare with master" })
+vim.keymap.set("n", "<leader>gr", ":DiffviewOpen origin/main<CR>", { desc = "Compare with main" })
