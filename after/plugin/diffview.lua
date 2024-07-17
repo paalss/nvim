55c6435276a91350ea94412868bfd352cad0721e
paalss <paalss94@gmail.com>
2024-07-17 23:49:19 +0200

    refactor(diffview): move git log keymaps to top

--- after/plugin/diffview.lua
+++ after/plugin/diffview.lua
@@ -1,10 +1,10 @@
--- Git log
-vim.keymap.set("n", "<leader>glo", ":DiffviewFileHistory<CR>", { desc = "Show git history" })
-vim.keymap.set("n", "<leader>glgp", ":DiffviewFileHistory %<CR>", { desc = "Show git history for current file" })
-
 -- Git status and close
 vim.keymap.set("n", "<leader>gg", ":DiffviewOpen<CR>", { desc = "Open git status (compate current index)" })
 vim.keymap.set("n", "<leader>gh", ":DiffviewFocusFiles<CR><C-w>l:DiffviewClose<CR>", { desc = "Close Diffview" })
+
+-- Git log
+vim.keymap.set("n", "<leader>glo", ":DiffviewFileHistory<CR>", { desc = "Show git history" })
+vim.keymap.set("n", "<leader>glgp", ":DiffviewFileHistory %<CR>", { desc = "Show git history for current file" })
 
 -- Compare working index with branch
 vim.keymap.set("n", "<leader>gy", ":DiffviewOpen origin/master<CR>", { desc = "Compare with master" })
