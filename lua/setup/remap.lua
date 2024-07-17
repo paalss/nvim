b6528ec32798add23f9c10ff1718cf86e4594100
paalss <paalss94@gmail.com>
2024-07-17 22:22:22 +0200

    fix(remap): better delete-keymaps

--- lua/setup/remap.lua
+++ lua/setup/remap.lua
@@ -54,8 +54,8 @@ vim.keymap.set("x", "<leader>p", "\"_dP")
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
-vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
+vim.keymap.set("n", "<leader>de", "\"_d")
+vim.keymap.set("v", "<leader>de", "\"_d")
 
 -- -- all text
 vim.keymap.set("n", "<leader>vall", "ggVG", { desc = "mark all" })
