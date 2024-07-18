bb571a9589612a251f381c1c834e13b562d60efd
paalss <paalss94@gmail.com>
2024-07-18 18:51:02 +0200

    feat(diffview): scoped-keymap for closing diffview

--- after/plugin/diffview.lua
+++ after/plugin/diffview.lua
@@ -2,11 +2,15 @@ local diffview = require 'diffview'
 
 diffview.setup {
   keymaps = {
+    view = {
+      { "n", "q", ":DiffviewClose<CR>", { desc = "Close Diffview" } },
+    },
     file_panel = {
       { "n", "cc",  ":Git commit<CR>",                   { desc = "Commit" } },
       { "n", "caa", ":Git commit --amend<CR>",           { desc = "Commit amend" } },
       { "n", "can", ":Git commit --amend --no-edit<CR>", { desc = "Commit amend no-edit" } },
-    }
+      { "n", "q",   "<C-w>l:DiffviewClose<CR>",          { desc = "Close Diffview" } },
+    },
   }
 }
 
