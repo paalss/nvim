0e75808f8c767885abb1e3d4e5c5afbf8546d5c3
paalss <paalss94@gmail.com>
2024-07-21 14:21:58 +0200

    feat(fugitive): keymap for opening diff window

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -2,3 +2,4 @@ vim.keymap.set("n", "<leader>gt", ":Git<CR>", { desc = "Open Git – Fugitive" }
 vim.keymap.set("n", "<leader>gf", ":Gwrite<CR>", { desc = "Save and stage/add buffer-file to git" })
 vim.keymap.set("n", "<leader>ga", ":wa<CR>:G add -A<CR>", { desc = "Stage/add all files to git" })
 vim.keymap.set("n", "<leader>gc", ":G commit<CR>", { desc = "Git commit" })
+vim.keymap.set("n", "<leader>g5", ":Gdiffsplit<CR>", { desc = "Open git status (Fugitive)" }) -- TODO: disable if superseded by diffview
