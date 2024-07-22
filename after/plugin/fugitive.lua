56712a919676ce26a7485c4b40293d08406adcc6
paalss <paalss94@gmail.com>
2024-07-22 20:09:21 +0200

    style(fugitive): maybe remove comment

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,5 +1,5 @@
 vim.keymap.set("n", "<leader>gt", ":Git<CR>", { desc = "Open Git – Fugitive" }) -- TODO: disable if it's unnecessary
-vim.keymap.set("n", "<leader>gf", ":Gwrite<CR>", { desc = "Save and stage/add buffer-file to git" })
+vim.keymap.set("n", "<leader>gf", ":Gwrite<CR>", { desc = "Write and stage buffer" }) -- TODO: disable if superseded by gitsign's stage_buffer
 vim.keymap.set("n", "<leader>ga", ":wa<CR>:G add -A<CR>", { desc = "Stage/add all files to git" })
 vim.keymap.set("n", "<leader>gc", ":G commit<CR>", { desc = "Git commit" })
 vim.keymap.set("n", "<leader>g5", ":Gdiffsplit<CR>", { desc = "Open git status (Fugitive)" }) -- TODO: disable if superseded by diffview
