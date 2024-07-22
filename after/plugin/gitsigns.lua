43d4eee927e16e06d684c696be42df30eab3dd41
paalss <paalss94@gmail.com>
2024-07-22 20:11:22 +0200

    fix(gitsigns): better descriptions

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -22,18 +22,18 @@ require('gitsigns').setup {
     end, { desc = "prev diff hunk", expr = true })
 
     -- Actions
-    map('n', '<leader>hs', gs.stage_hunk, { desc = "stage hunk" })
-    map('n', '<leader>hr', gs.reset_hunk, { desc = "reset hunk" })
-    map('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end, { desc = "stage hunk" })
-    map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end, { desc = "reset hunk" })
-    map('n', '<leader>hS', gs.stage_buffer, { desc = "stage buffer" })
-    map('n', '<leader>hu', gs.undo_stage_hunk, { desc = "undo stage hunk" })
+    map('n', '<leader>hs', gs.stage_hunk, { desc = "Stage hunk" })
+    map('n', '<leader>hr', gs.reset_hunk, { desc = "Reset hunk" })
+    map('n', '<leader>hu', gs.undo_stage_hunk, { desc = "Unstage hunk" })
+    map('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end, { desc = "Stage hunk" })
+    map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end, { desc = "Reset hunk" })
+    map('n', '<leader>hS', gs.stage_buffer, { desc = "Stage buffer" })
     map('n', '<leader>hR', gs.reset_buffer)
-    map('n', '<leader>hp', gs.preview_hunk, { desc = "preview hunk" })
+    map('n', '<leader>hp', gs.preview_hunk, { desc = "Preview hunk" })
     map('n', '<leader>hb', function() gs.blame_line { full = true } end)
     map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = "Show git blame on line" })
     map('n', '<leader>hd', gs.diffthis)
-    map('n', '<leader>hD', function() gs.diffthis('~') end, { desc = "diffthis 2" })
+    map('n', '<leader>hD', function() gs.diffthis('~') end, { desc = "Diffthis 2" })
     map('n', '<leader>td', gs.toggle_deleted)
 
     -- Text object
