b553ea1dd1ef3566b03d00efe7412e35fc0b8807
Pål Stakvik <pal.stakvik@visma.com>
2024-07-25 17:20:20 +0200

    feat(gitsigns): keymaps for unstaging and resetting lines

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -22,14 +22,23 @@ require('gitsigns').setup {
     end, { desc = "prev diff hunk", expr = true })
 
     -- Actions
+
+    -- -- stage, unstage, reset
     map('n', '<leader>hs', gs.stage_hunk, { desc = "Stage hunk" })
-    map('n', '<leader>ls', "V:Gitsigns stage_hunk", { desc = "Stage hunk" })
-    map('n', '<leader>hr', gs.reset_hunk, { desc = "Reset hunk" })
     map('n', '<leader>hu', gs.undo_stage_hunk, { desc = "Unstage hunk" })
+    map('n', '<leader>hr', gs.reset_hunk, { desc = "Reset hunk" })
+
+    map('n', '<leader>ls', "V:Gitsigns stage_hunk", { desc = "Stage line" })
+    map('n', '<leader>lu', "V:Gitsigns undo_stage_hunk", { desc = "Unstage line" })
+    map('n', '<leader>lu', "V:Gitsigns reset_hunk", { desc = "Reset line" })
+
     map('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end, { desc = "Stage hunk" })
     map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end, { desc = "Reset hunk" })
+
     map('n', '<leader>hS', gs.stage_buffer, { desc = "Stage buffer" })
-    map('n', '<leader>hR', gs.reset_buffer)
+    map('n', '<leader>hR', gs.reset_buffer, { desc = "Reset buffer" })
+
+    -- -- view
     map('n', '<leader>hp', gs.preview_hunk, { desc = "Preview hunk" })
     map('n', '<leader>hb', function() gs.blame_line { full = true } end)
     map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = "Show git blame on line" })
