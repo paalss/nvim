d97f11ee4f157b901b76fe8de5bd49f1280461fb
paalss <paalss94@gmail.com>
2024-07-28 13:03:52 +0200

    feat(tmux): keymap for updating ~/.tmux.conf

--- lua/tmux.lua
+++ lua/tmux.lua
@@ -1,1 +1,2 @@
 -- vim.keymap.set("n", "<C-m>", "<cmd>:silent !tmux split-window -v<CR>", { desc = "TMUX: 'Open terminal': new horizontal tmux pane" })
+vim.keymap.set("n", "<leader><leader>tmux", ":! cp ~/.config/nvim/.tmux.conf ~/.tmux.conf")
