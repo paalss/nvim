f91155dddbc7bb9cff05f66f4a0a1fe437b8ffa3
paalss <paalss94@gmail.com>
2024-07-31 22:14:06 +0200

    feat(menu): keymap for opening menu
    and disable vim-menu built-in keymap with <leader>m shortcut

---
+++ after/plugin/menu.lua
@@ -0,0 +1,5 @@
+vim.keymap.set("n", "<leader>m", "<nop>", { desc = "disabled" }) -- disable leader m (comes automatically with vim-menu, but it clashes with <leader>mark)
+vim.keymap.set("n", "<A-a>", ":Menu<CR>", { desc = "Open menu" })
+
+-- add menu item: `:menu Git.compare\ with\ branch.master \gy`
+-- delete menu item: `:unmenu Git.compare\ with\ branch.master`
