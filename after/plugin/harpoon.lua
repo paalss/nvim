27389f021b818a243c1b0a9431c272ccd206b3fe
paalss <paalss94@gmail.com>
2024-08-03 18:40:29 +0200

    fix(harpoon): change harpoon shortcut
    shift e is already allocated in vim

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -5,7 +5,7 @@ local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>ha", mark.add_file, { desc = "Add to Harpoon" })
-vim.keymap.set("n", "<S-e>", ui.toggle_quick_menu, { desc = "View Harpoon" })
+vim.keymap.set("n", "<A-e>", ui.toggle_quick_menu, { desc = "View Harpoon" })
 
 vim.keymap.set("n", "<leader><leader>hd", function ()
  ui.toggle_quick_menu()
