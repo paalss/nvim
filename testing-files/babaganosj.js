db539d314c16354fd86ab5d98565bbed93e94f0b
paalss <paalss94@gmail.com>
2024-08-22 22:40:26 +0200

    chore: uglify

--- testing-files/babaganosj.js
+++ testing-files/babaganosj.js
@@ -1,15 +1,16 @@
-const { log } = require("console")
+const { log} = require("console")
 
 function babaganosj() {
-    console.log('hallo på do folkens')
-    log('hello folkens')
+console.log('hallo på do folkens')
+        log('hello folkens')
 }
 
 babaganosj()
 
 const detteErEnLitenSpoek = () => {
     // som blir ødelagt hvert forsøk
-}
+};
 
 const bool = true
 let string = "heeeiååå"
+let aaaaaa = 'aaaaaaaaaa'
