d8c7ce83403196c5cfeb5440c83c2cd226b918d7
Pål Stakvik <pal.stakvik@visma.com>
2024-08-23 17:49:17 +0200

    fix(neotree): make window float

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -1,3 +1,3 @@
-vim.keymap.set("n", "<A-r>", "<cmd>Neotree toggle reveal<cr>", { desc = "Toggle file explorer" })
+vim.keymap.set("n", "<A-r>", "<cmd>Neotree toggle reveal float<cr>", { desc = "Toggle file explorer" })
 vim.keymap.set("n", "<leader>e3", "<cmd>Neotree close<cr>", { desc = "Close Neotree" })
 vim.keymap.set("n", "<leader>gss", "<cmd>Neotree git_status<cr>", { desc = "Show uncommited changes" })
