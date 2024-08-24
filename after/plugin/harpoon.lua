6ede9add4c87bc84b45184d72750569e762afd4e
paalss <paalss94@gmail.com>
2024-08-24 15:59:53 +0200

    fix(harpoon): update to harpoon2 syntax

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,31 +1,22 @@
-require("harpoon").setup({
-  tabline = true,
-})
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
+local harpoon = require("harpoon")
 
-vim.keymap.set("n", "<leader>ha", mark.add_file, { desc = "Add to Harpoon" })
-vim.keymap.set("n", "<A-e>", ui.toggle_quick_menu, { desc = "View Harpoon" })
+-- REQUIRED
+harpoon:setup({})
+-- REQUIRED
 
-vim.keymap.set("n", "<leader><leader>hd", function ()
- ui.toggle_quick_menu()
- vim.cmd(":%d")
-end, { desc = "Delete all Harpoon marks"})
+vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "Add to Harpoon" })
+vim.keymap.set("n", "<A-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "View Harpoon" })
 
-vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end, { desc = "Open Harpoon file #1" })
-vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end, { desc = "Open Harpoon file #2" })
-vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end, { desc = "Open Harpoon file #3" })
-vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end, { desc = "Open Harpoon file #4" })
-vim.keymap.set("n", "<leader>5", function() ui.nav_file(5) end, { desc = "Open Harpoon file #5" })
-vim.keymap.set("n", "<leader>6", function() ui.nav_file(6) end, { desc = "Open Harpoon file #6" })
-vim.keymap.set("n", "<leader>7", function() ui.nav_file(7) end, { desc = "Open Harpoon file #7" })
-vim.keymap.set("n", "<leader>8", function() ui.nav_file(8) end, { desc = "Open Harpoon file #8" })
-vim.keymap.set("n", "<leader>9", function() ui.nav_file(9) end, { desc = "Open Harpoon file #9" })
+vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Open Harpoon file #1" })
+vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Open Harpoon file #2" })
+vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Open Harpoon file #3" })
+vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Open Harpoon file #4" })
+vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end, { desc = "Open Harpoon file #5" })
+vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end, { desc = "Open Harpoon file #6" })
+vim.keymap.set("n", "<leader>7", function() harpoon:list():select(7) end, { desc = "Open Harpoon file #7" })
+vim.keymap.set("n", "<leader>8", function() harpoon:list():select(8) end, { desc = "Open Harpoon file #8" })
+vim.keymap.set("n", "<leader>9", function() harpoon:list():select(9) end, { desc = "Open Harpoon file #9" })
 
-vim.keymap.set("n", "<A-,>", function() ui.nav_prev() end, { desc = "Go to next Harpoon file" })
-vim.keymap.set("n", "<A-.>", function() ui.nav_next() end, { desc = "Go to prev Harpoon file" })
-
-vim.cmd('highlight! HarpoonInactive guibg=NONE guifg=#63698c')
--- vim.cmd('highlight! HarpoonNumberInactive guibg=NONE guifg=#7aa2f7')
-vim.cmd('highlight! HarpoonActive guibg=NONE guifg=white')
-vim.cmd('highlight! HarpoonNumberActive guibg=NONE guifg=#7aa2f7')
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<A-,>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<A-.>", function() harpoon:list():next() end)
