869e5074a0a4626405bd5fb8fd70c5fa265b22ee
paalss <paalss94@gmail.com>
2024-09-08 18:23:23 +0200

    feat(diffview): add quit-keymap to file browser as well

--- after/plugin/diffview.lua
+++ after/plugin/diffview.lua
@@ -8,17 +8,18 @@ diffview.setup {
       { "n", "<S-u>", "V:diffget<CR>",      { desc = "Unstage line" } },
       { "v", "<S-s>", ":diffput<CR>",       { desc = "Stage selection" } },
       { "v", "<S-u>", ":diffget<CR>",       { desc = "Unstage selection" } },
-      { "n", "dp",    "dp",                 { desc = "Stage hunk" } },       -- add desc to existing map
-      { "n", "do",    "do",                 { desc = "Unstage hunk" } },     -- add desc to existing map
+      { "n", "dp",    "dp",                 { desc = "Stage hunk" } },   -- add desc to existing map
+      { "n", "do",    "do",                 { desc = "Unstage hunk" } }, -- add desc to existing map
     },
     file_panel = {
-      { "n", "cc", ":Git commit<CR>",                   { desc = "Commit" } },
-      { "n", "ca", ":Git commit --amend<CR>",           { desc = "Commit amend" } },
-      { "n", "ce", ":Git commit --amend --no-edit<CR>", { desc = "Commit amend no-edit" } },
-      { "n", "q",  "<C-w>l:DiffviewClose<CR>",          { desc = "Close Diffview" } },
+      { "n", "<A-q>", ":DiffviewClose<CR>",                { desc = "Close Diffview" } },
+      { "n", "cc",    ":Git commit<CR>",                   { desc = "Commit" } },
+      { "n", "ca",    ":Git commit --amend<CR>",           { desc = "Commit amend" } },
+      { "n", "ce",    ":Git commit --amend --no-edit<CR>", { desc = "Commit amend no-edit" } },
+      { "n", "q",     "<C-w>l:DiffviewClose<CR>",          { desc = "Close Diffview" } },
     },
     file_history_panel = {
-      { "n", "<A-q>", ":DiffviewClose<CR>", { desc = "Close Diffview" } },
+      { "n", "<A-q>",            ":DiffviewClose<CR>",                         { desc = "Close Diffview" } },
       -- find commit hash regardless of cursor positioning setup:
 
       -- $file<CR>f|;w ____ breakdown
