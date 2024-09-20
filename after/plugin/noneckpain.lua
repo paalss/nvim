1560bdd7c4ac5595608c81952a99f10e1762227e
paalss <paalss94@gmail.com>
2024-09-20 21:43:07 +0200

    fix(noneckpain): wider default width

--- after/plugin/noneckpain.lua
+++ after/plugin/noneckpain.lua
@@ -1,5 +1,9 @@
+require('no-neck-pain').setup({
+  width = 145
+})
+
 vim.keymap.set("n", "<leader>bb", ":NoNeckPain<CR>", { desc = "Center current buffer (noneckpain)" })
-vim.keymap.set("n", "<leader>bg", ":NoNeckPainResize 100<CR>", { desc = "Noneckpain: Default width" })
+vim.keymap.set("n", "<leader>bg", ":NoNeckPainResize 145<CR>", { desc = "Noneckpain: Default width" })
 vim.keymap.set("n", "<leader>bl", ":NoNeckPainToggleRightSide<CR>", { desc = "Noneckpain: Toggle right buffer" })
 vim.keymap.set("n", "<A-Up>", ":NoNeckPainWidthUp<CR>", { desc = "Noneckpain: Increase width" })
 vim.keymap.set("n", "<A-Down>", ":NoNeckPainWidthDown<CR>", { desc = "Noneckpain: Decrease width" })
