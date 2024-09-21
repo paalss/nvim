26d63c4e4389bad15f75d55d6a51e0d6c0018b90
paalss <paalss94@gmail.com>
2024-09-21 16:24:24 +0200

    feat(quickui): add shortcut to set paste toggle

--- after/plugin/quickui.lua
+++ after/plugin/quickui.lua
@@ -1,6 +1,5 @@
 vim.cmd [[
 
-
 " &Menu = press m to pick
 " M&enu = press e to pick
 " [ "&Menu item", "Function", "Hover-tip"]
@@ -8,31 +7,11 @@ vim.cmd [[
 " clear all the menus
 call quickui#menu#reset()
 
-" install a 'File' menu, use [text, command] to represent an item.
-call quickui#menu#install('&File', [
-            \ [ "&New File\tCtrl+n", 'echo 0' ],
-            \ [ "&Open File\t(F3)", 'echo 1' ],
-            \ [ "&Close", 'echo 2' ],
-            \ [ "--", '' ],
-            \ [ "&Save\tCtrl+s", 'echo 3'],
-            \ [ "Save &As", 'echo 4' ],
-            \ [ "Save All", 'echo 5' ],
-            \ [ "--", '' ],
-            \ [ "E&xit\tAlt+x", 'echo 6' ],
-            \ ])
-
-" items containing tips, tips will display in the cmdline
-call quickui#menu#install('&Edit', [
-            \ [ '&Copy', 'echo 1', 'tip 1' ],
-            \ [ '&Paste', 'echo 2', 'tip 2' ],
-            \ [ '&Find', 'echo 3', 'tip 3' ],
-            \ ])
-
 " script inside %{...} will be evaluated and expanded in the string
+
 call quickui#menu#install("&Option", [
-			\ ['Set &Spell %{&spell? "Off":"On"}', 'set spell!'],
-			\ ['Set &Cursor Line %{&cursorline? "Off":"On"}', 'set cursorline!'],
-			\ ['Set &Paste %{&paste? "Off":"On"}', 'set paste!'],
+			\ ["Set &Paste %{&paste? 'Off':'On'}\tSPC sto", "set paste!","%{&paste? 'Turn on for coding':'Turn on for pasting from OS registry'}"],
+      \ [ "--", '' ],
 			\ ])
 
 " register HELP menu with weight 10000
@@ -61,3 +40,14 @@ let g:quickui_show_tip = 1
 
 vim.keymap.set("n", "<A-s>", ":call quickui#menu#open()<CR>", { desc = "Open menu (Quickui)" })
 
+
+
+-- dsfsd
+-- fdsfsd
+--
+--
+-- set paste
+
+
+
+--
