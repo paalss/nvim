94b357571f9c4bc6b6e2bef58c3531de88a51a6c
paalss <paalss94@gmail.com>
2024-11-03 17:14:08 +0100

    feat(lazygit): add copy keymap

--- after/plugin/lazygit.lua
+++ after/plugin/lazygit.lua
@@ -1,4 +1,5 @@
 vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", { desc = "Open Lazygit" })
+vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>", { desc = "Open Lazygit copy" })
 vim.g.lazygit_use_custom_config_file_path = 1                   -- 1 = enable custom file path
 vim.g.lazygit_config_file_path = '~/.config/lazygit/config.yml' -- where config file is storend config file
 
