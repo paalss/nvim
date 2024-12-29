6af50b9e0cfbc3f26079b2ee4c7fe82123a3d1d8
paalss <paalss94@gmail.com>
2024-12-29 19:06:19 +0100

    fix: make file tree sidebar by default

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -1,3 +1,4 @@
-vim.keymap.set("n", "<A-r>", "<cmd>Neotree toggle reveal float<cr>", { desc = "Toggle file explorer" })
+vim.keymap.set("n", "<A-r>", "<cmd>Neotree toggle reveal<cr>", { desc = "Toggle file explorer" })
+vim.keymap.set("n", "<leader><A-r>", "<cmd>Neotree toggle reveal float<cr>", { desc = "Toggle file explorer (float)" })
 vim.keymap.set("n", "<leader>e3", "<cmd>Neotree close<cr>", { desc = "Close Neotree" })
 vim.keymap.set("n", "<leader>gss", "<cmd>Neotree git_status<cr>", { desc = "Show uncommited changes" })
