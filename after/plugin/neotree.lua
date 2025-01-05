a941845ee582b1acf7e524bf8c58e0d1db1514e4
paalss <paalss94@gmail.com>
2025-01-05 15:33:13 +0100

    feat(neotree): close if last window

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -1,3 +1,6 @@
+require("neo-tree").setup({
+  close_if_last_window = true
+})
 vim.keymap.set("n", "<A-r>", "<cmd>Neotree toggle reveal<cr>", { desc = "Toggle file explorer" })
 vim.keymap.set("n", "<leader><A-r>", "<cmd>Neotree toggle reveal float<cr>", { desc = "Toggle file explorer (float)" })
 vim.keymap.set("n", "<leader>e3", "<cmd>Neotree close<cr>", { desc = "Close Neotree" })
