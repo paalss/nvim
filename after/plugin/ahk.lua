e01592aa3875a4f439ecb0cd449e5cf4f8620caf
paalss <paalss94@gmail.com>
2025-02-15 10:25:03 +0100

    fix: move start-ahk.sh to ~/

--- after/plugin/ahk.lua
+++ after/plugin/ahk.lua
@@ -1,5 +1,5 @@
 -- keymaps for autohotkey
 
-vim.keymap.set("n", "<leader>ahk", ":!timeout 0.1s bash ~/.config/nvim/lua/autohotkey/start-ahk.sh<CR><CR>", {
+vim.keymap.set("n", "<leader>ahk", ":!timeout 0.1s bash ~/start-ahk.sh<CR><CR>", {
   desc = "Start autohotkey"
 })
--- lua/autohotkey/start-ahk.sh
+++
@@ -1,1 +0,0 @@
-'/mnt/c/Program Files/AutoHotkey/v2/AutoHotkey.exe' ~/autohotkey.ahk
