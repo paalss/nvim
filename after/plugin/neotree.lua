914ac75a1fc59e0417a2e41bb94ff1bd02154425
paalss <paalss94@gmail.com>
2025-06-07 12:32:09 +0200

    neotree: show dotfiles in different color and don't hijack netrw

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -4,9 +4,24 @@ require("neo-tree").setup({
     mappings = {
       ["l"] = "open"
     }
+  },
+  filesystem = {
+    filtered_items = {
+      visible = true
+      -- hide_dotfiles = false,
+      -- hide_gitignored = false
+    },
+    hide_by_name = {
+      -- "node_modules"
+    },
+    follow_current_file = {
+      enabled = true,
+      leave_dirs_open = true
+    },
+    hijack_netrw_behavior = "disabled",
   }
 })
-vim.keymap.set("n", "<A-r>", "<cmd>Neotree toggle reveal left<cr>", { desc = "Toggle file explorer" })
+vim.keymap.set("n", "<A-r>", "<cmd>Neotree toggle left<cr>", { desc = "Toggle file explorer" })
 -- vim.keymap.set("n", "<A-r>h", "<cmd>Neotree toggle reveal left<cr>", { desc = "Toggle file explorer (left sidebar)" })
 -- vim.keymap.set("n", "<A-r>k", "<cmd>Neotree toggle reveal float<cr>", { desc = "Toggle file explorer (float)" })
 -- vim.keymap.set("n", "<A-r>l", "<cmd>Neotree toggle reveal current<cr>", { desc = "Toggle file explorer (current)" })
