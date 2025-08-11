eba4e64994de2e0929fc18c9d8028fedfea7cd37
Pål Stakvik <pal.stakvik@visma.com>
2025-08-11 17:04:42 +0200

    reduce c-e and c-y distance

--- lua/setup/remap.lua
+++ lua/setup/remap.lua
@@ -223,8 +223,8 @@ vim.keymap.set("n", "<leader><leader>dca", "gg/#<CR>Vggy:cq<CR>",
 vim.keymap.set("n", "<C-d>", "23j", { desc = "Scroll page down" })
 vim.keymap.set("n", "<C-u>", "23k", { desc = "Scroll page up" })
 
-vim.keymap.set("n", "<C-e>", "11j", { desc = "Scroll down" })
-vim.keymap.set("n", "<C-y>", "11k", { desc = "Scroll up" })
+vim.keymap.set("n", "<C-e>", "8j", { desc = "Scroll down" })
+vim.keymap.set("n", "<C-y>", "8k", { desc = "Scroll up" })
 
 
 -- GIT NAVIGATION
