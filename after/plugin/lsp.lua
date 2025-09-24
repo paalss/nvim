23c5a9b91b86ac8b1e4ab17d50c983aca6e54f7d
Pål Stakvik <pal.stakvik@visma.com>
2025-09-24 14:15:49 +0200

    nice keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -121,6 +121,8 @@ lsp.on_attach(function(client, bufnr)
     })
   vim.keymap.set("n", "<C-i>l", function() vim.diagnostic.goto_next() end, { buffer = bufnr, remap = false, desc = "Go to next error" })
   vim.keymap.set("n", "<C-i>h", function() vim.diagnostic.goto_prev() end, { buffer = bufnr, remap = false, desc = "Go to previous error" })
+  vim.keymap.set("n", "<A-l>", function() vim.diagnostic.goto_next() end, { buffer = bufnr, remap = false, desc = "Go to next error" })
+  vim.keymap.set("n", "<A-h>", function() vim.diagnostic.goto_prev() end, { buffer = bufnr, remap = false, desc = "Go to previous error" })
 
   -- do something
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end,
