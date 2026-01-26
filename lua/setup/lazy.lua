6803fd43f116f7d4b9d70d64b2a36803a6df4b1c
Pål Stakvik <pal.stakvik@visma.com>
2026-01-26 17:35:39 +0100

    remove lsp-zero

--- lua/setup/lazy.lua
+++ lua/setup/lazy.lua
@@ -145,25 +145,6 @@ local plugins = {
       vim.fn["mkdp#util#install"]()
     end
   },
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
-    pin = true,
-    dependencies = {               -- LSP Support
-      { 'neovim/nvim-lspconfig' }, -- Required
-      {
-        -- Optional
-        'williamboman/mason.nvim',
-        build = function()
-          pcall(vim.cmd, 'MasonUpdate')
-        end
-      }, { 'williamboman/mason-lspconfig.nvim' }, -- Optional
-      -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },                     -- Required
-      { 'hrsh7th/cmp-nvim-lsp' },                 -- Required
-      { 'L3MON4D3/LuaSnip' }                      -- Required
-    }
-  },
 }
 
 require("lazy").setup(plugins, {})
