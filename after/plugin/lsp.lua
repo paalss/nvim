4f66eb4d8519187d9c4083954b6fa89f127d38a5
Pål Stakvik <pal.stakvik@visma.com>
2026-01-27 09:11:17 +0100

    feat: activate javascript / typescript / react lsp!

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,11 +3,29 @@ vim.lsp.config('luals', {
   filetypes = {'lua'},
   root_markers = {'.luarc.json', '.luarc.jsonc'},
 })
-vim.lsp.config('tsserver', {
-  -- cmd = {'lua-language-server'},
-  filetypes = {'js', 'ts', 'jsx', 'tsx'},
-  root_markers = {},
+
+-- Set configuration for typescript language server
+vim.lsp.config('ts_ls', {
+  cmd = { 'typescript-language-server', '--stdio' },
+  filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
 })
 
-
 vim.lsp.enable('luals')
+vim.lsp.enable('ts_ls')
+
+-- andre forsøk
+--
+-- vim.lsp.config('tsserver', {
+--   cmd = {'typescript-language-server'},
+--   filetypes = {'js', 'ts', 'jsx', 'tsx'},
+--   root_markers = {},
+-- })
+
+-- vim.lsp.config('js', {
+--   cmd = {'javascript-language-server'},
+--   filetypes = {'js', 'jsx'},
+--   root_markers = {'.luarc.json', '.luarc.jsonc'},
+-- })
+
+-- vim.lsp.enable('tsserver')
+-- vim.lsp.enable('js')
