a0e5f4dc51de0d7e1dc5ebf67e9d1aeabd44c13b
paalss <paalss94@gmail.com>
2026-02-07 19:06:49 +0100

    add things

--- lua/setup/remap.lua
+++ lua/setup/remap.lua
@@ -207,6 +207,7 @@ vim.keymap.set("n", "<leader>cå", ":%s/├Ñ/å/g<CR>", { desc = "Correct å" }
 --------------------------------------------------------
 
 vim.cmd[[iabbrev locaing loading]]
+vim.cmd[[iabbrev timezpne timezone]]
 
 
 --------------------------------------------------------
@@ -326,6 +327,7 @@ vim.keymap.set({ "n", "v" }, "<C-y>", "8k", { desc = "Scroll up" })
 -- SEARCH & JUMP-TO
 
 vim.keymap.set("n", "<leader>n", "/", { desc = "Search forward" })
+vim.keymap.set("v", "<leader>n", "/", { desc = "Search forward" })
 vim.keymap.set("n", "<leader><leader>n", "?", { desc = "Search backward" })
 vim.keymap.set("n", "<C-f>", "/", { desc = "Search forward" })
 vim.keymap.set("n", "<leader><C-f>", "?", { desc = "Search backward" })
