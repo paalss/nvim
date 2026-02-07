eeb54300ea3052f162bd49c8e7af6f04983b7d71
paalss <paalss94@gmail.com>
2026-02-07 19:06:56 +0100

    reposition code to mac-develop version

--- after/plugin/diffview.lua
+++ after/plugin/diffview.lua
@@ -18,10 +18,10 @@ diffview.setup {
     },
     file_panel = {
       { "n", "<A-q>", ":DiffviewClose<CR>",                { desc = "Close Diffview" } },
+      { "n", "q",     "<C-w>l:DiffviewClose<CR>",          { desc = "Close Diffview" } },
       { "n", "cc",    ":Git commit<CR>",                   { desc = "Commit" } },
       { "n", "ca",    ":Git commit --amend<CR>",           { desc = "Commit amend" } },
       { "n", "ce",    ":Git commit --amend --no-edit<CR>", { desc = "Commit amend no-edit" } },
-      { "n", "q",     "<C-w>l:DiffviewClose<CR>",          { desc = "Close Diffview" } },
     },
     file_history_panel = {
       { "n", "<A-q>",            ":DiffviewClose<CR>",                         { desc = "Close Diffview" } },
--- lua/setup/remap.lua
+++ lua/setup/remap.lua
@@ -49,40 +49,43 @@ vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Write/save" })
 vim.keymap.set("n", "<leader>W", ":wa<CR>", { desc = "Write all files" })
 -- vim.keymap.set("n", "<leader>x", ":x<CR>", { desc = "Write and quit" })
 vim.keymap.set("n", "<leader>so", ":so<CR>", { desc = "Source file" })
+
 vim.keymap.set({"n", "v"}, "<A-h>", "0", { desc = "Go to beginning of line" }) -- <A-h>
 vim.keymap.set({"n", "v"}, "<A-l>", "$", { desc = "Go to end of line" }) -- <A-l>
+
 vim.keymap.set({"n", "v"}, "gh", "0", { desc = "Go to beginning of line" })
 -- vim.keymap.set({"n", "v"}, "gl", "$", { desc = "Go to end of line" }) -- funker ikke i WSL
 
 vim.keymap.set("n", "<leader>g", "%", { desc = "%" })
-vim.keymap.set("n", "<leader>G", ":! git add %<CR>", { desc = "Stage current file" })
 vim.keymap.set("n", "<leader>c", "\"", { desc = "double quote" })
 vim.keymap.set("n", "<leader>x", "@", { desc = "@" })
 vim.keymap.set("n", "<leader>S", ":let @s = @*<CR>", { desc = "Save last paste item to 's'-registry "})
 vim.keymap.set("n", "<leader>L", "\"sp", { desc = "Paste from 's'-registry"})
 
 
+vim.keymap.set("n", "<leader>G", ":! git add %<CR>", { desc = "Stage current file" })
+
 --------------------------------------------------------
 -- OPERATOR PENDING
 --------------------------------------------------------
 
-vim.keymap.set("o", "ic", "i\"", { desc = "Inside \"" }) -- targets.vim: iq = inside any quote (single, double, backtick...)
+-- c -> "
+vim.keymap.set("o", "ic", "i\"", { desc = "Inside \"" })
 vim.keymap.set("o", "ac", "a\"", { desc = "Around \"" })
-
-vim.keymap.set("o", "iC", "i\'", { desc = "Inside \'" })
-vim.keymap.set("o", "aC", "a\'", { desc = "Around \'" })
-
-vim.keymap.set("o", "iv", "i[", { desc = "Inside [" })
-vim.keymap.set("o", "av", "a[", { desc = "Around [" })
-
 vim.keymap.set("v", "ic", "i\"", { desc = "Inside \"" })
 vim.keymap.set("v", "ac", "a\"", { desc = "Around \"" })
 
+-- C -> '
+vim.keymap.set("o", "iC", "i\'", { desc = "Inside \'" })
+vim.keymap.set("o", "aC", "a\'", { desc = "Around \'" })
 vim.keymap.set("v", "iC", "i\'", { desc = "Inside \'" })
 vim.keymap.set("v", "aC", "a\'", { desc = "Around \'" })
 
-vim.keymap.set("v", "iv", "i[", { desc = "Inside [" })
-vim.keymap.set("v", "av", "a[", { desc = "Around [" })
+-- x -> [
+vim.keymap.set("o", "ix", "i[", { desc = "Inside [" })
+vim.keymap.set("o", "ax", "a[", { desc = "Around [" })
+vim.keymap.set("v", "ix", "i[", { desc = "Inside [" })
+vim.keymap.set("v", "ax", "a[", { desc = "Around [" })
 
 -- "operator pending ish"
 
