80755f2f7505e7e70f4844188063d4923b50d020
Pål Stakvik <pal.stakvik@visma.com>
2026-02-10 17:33:43 +0100

    style: format file

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,9 +5,9 @@ luasnip.config.setup {}
 
 
 vim.lsp.config('luals', {
-  cmd = {'lua-language-server'},
-  filetypes = {'lua'},
-  root_markers = {'.luarc.json', '.luarc.jsonc'},
+  cmd = { 'lua-language-server' },
+  filetypes = { 'lua' },
+  root_markers = { '.luarc.json', '.luarc.jsonc' },
 })
 vim.lsp.enable('luals')
 
