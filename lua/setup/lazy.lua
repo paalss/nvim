b9a5b4657046a1b8845d550adf7d19bf51fa3322
paalss <paalss94@gmail.com>
2026-02-15 20:29:38 +0100

    restructure and format lazy.lua

--- lua/setup/lazy.lua
+++ lua/setup/lazy.lua
@@ -1,140 +1,101 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system(
-    { "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git",
-      "--branch=stable", -- latest stable release
+    { "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",    -- latest stable release
       lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
-
 
 vim.g.clipboard = {
   name = 'WslClipboard',
   copy = {
     ['+'] = 'clip.exe',
-    ['*'] = 'clip.exe',
+    ['*'] = 'clip.exe'
   },
   paste = {
     ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))'
   },
-  cache_enabled = 0,
+  cache_enabled = 0
 }
 
 vim.g.mapleader = " "
 
-local plugins = {
-  -- { 'wellle/targets.vim' }, -- har en bug hvor "b" targeter hvilken som helst bracket - (, { og [
-
-  --** LSP/autocompletion **--
-  -- {
-  --   -- LSP Configuration & Plugins
-  --   'neovim/nvim-lspconfig',
-  --   pin = true,
-  --   dependencies = {
-  --     -- Automatically install LSPs to stdpath for neovim
-  --     { 'williamboman/mason.nvim', config = true },
-  --     'williamboman/mason-lspconfig.nvim',
-
-  --     -- Useful status updates for LSP
-  --     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-  --     { 'j-hui/fidget.nvim',       tag = 'legacy', opts = {} },
-
-  --     -- Additional lua configuration, makes nvim stuff amazing!
-  --     'folke/neodev.nvim',
-  --   },
-  -- },
+local plugins = { -- { 'wellle/targets.vim' }, -- har en bug hvor "b" targeter hvilken som helst bracket - (, { og [
+  -- ** LSP/autocompletion **--
   {
-    -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    pin = true,
-    dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-
-      -- Adds LSP completion capabilities
-      'hrsh7th/cmp-nvim-lsp',
-
-      -- Path
-      'hrsh7th/cmp-path',
-
-      -- Adds a number of user-friendly snippets
-      'paalss/friendly-snippets',
-    },
-  },
-  -- {
-  --   'VonHeikemen/lsp-zero.nvim',
-  --   branch = 'v2.x',
-  --   pin = true,
-  --   dependencies = {               -- LSP Support
-  --     { 'neovim/nvim-lspconfig' }, -- Required
-  --     {
-  --       -- Optional
-  --       'williamboman/mason.nvim',
-  --       build = function()
-  --         pcall(vim.cmd, 'MasonUpdate')
-  --       end
-  --     }, { 'williamboman/mason-lspconfig.nvim' }, -- Optional
-  --     -- Autocompletion
-  --     { 'hrsh7th/nvim-cmp' },                     -- Required
-  --     { 'hrsh7th/cmp-nvim-lsp' },                 -- Required
-  --     { 'L3MON4D3/LuaSnip' }                      -- Required
-  --   }
-  -- },
-
-  --** Autopairs/surround **--
-  { 'tpope/vim-surround',  pin = true },
+    -- LSP Configuration & Plugins
+    'neovim/nvim-lspconfig',
+    dependencies = { -- Automatically install LSPs to stdpath for neovim
+      {
+        'williamboman/mason.nvim',
+        config = true
+      }, 'williamboman/mason-lspconfig.nvim', -- Useful status updates for LSP
+      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+      {
+        'j-hui/fidget.nvim',
+        tag = 'legacy',
+        opts = {}
+      }, -- Additional lua configuration, makes nvim stuff amazing!
+      'folke/neodev.nvim' }
+  }, {
+  -- Autocompletion
+  'hrsh7th/nvim-cmp',
+  pin = true,
+  dependencies = {                                  -- Snippet Engine & its associated nvim-cmp source
+    'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip', -- Adds LSP completion capabilities
+    'hrsh7th/cmp-nvim-lsp',                         -- Path
+    'hrsh7th/cmp-path',                             -- Adds a number of user-friendly snippets
+    'paalss/friendly-snippets' }
+},                                                  -- ** Autopairs/surround **--
   {
-    'alvan/vim-closetag',
+    'tpope/vim-surround',
     pin = true
-  },
-  { 'adelarsq/vim-matchit' },
+  }, {
+  'alvan/vim-closetag',
+  pin = true
+}, { 'adelarsq/vim-matchit' }, {
+  "windwp/nvim-autopairs",
+  pin = true,
+  config = function()
+    require("nvim-autopairs").setup {}
+  end
+}, -- ** Colorschemes **--
   {
-    "windwp/nvim-autopairs",
-    pin = true,
-    config = function()
-      require("nvim-autopairs").setup {}
-    end
-  },
-
-  --** Colorschemes **--
-  { 'bignimbus/pop-punk.vim',        pin = true },
+    'bignimbus/pop-punk.vim',
+    pin = true
+  }, {
+  "folke/tokyonight.nvim",
+  pin = true,
+  lazy = false,
+  priority = 1000,
+  opts = {}
+}, -- ** Fuzzy finder **--
   {
-    "folke/tokyonight.nvim",
-    pin = true,
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
-
-  --** Fuzzy finder **--
-  { "junegunn/fzf", build = "./install --bin"  },
-  { "junegunn/fzf.vim" },
-  {
-    'nvim-telescope/telescope.nvim', version = '*',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        -- optional but recommended
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    }
-  },
-
-  --** File navigation **--
+    "junegunn/fzf",
+    build = "./install --bin"
+  }, { "junegunn/fzf.vim" }, {
+  'nvim-telescope/telescope.nvim',
+  enabled = true,
+  version = '*',
+  dependencies = { 'nvim-lua/plenary.nvim',  -- optional but recommended
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make'
+    } }
+}, -- { "MaximilianLloyd/adjacent.nvim", pin = true },
+  -- ** File navigation **--
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     enabled = true,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim"                                                -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     }
-  },
-  { "MaximilianLloyd/adjacent.nvim", pin = true },
-  { 'sindrets/diffview.nvim',        pin = true },
-  -- {
+  }, {
+  'sindrets/diffview.nvim',
+  pin = true
+}, -- {
   --   "ThePrimeagen/harpoon",
   --   commit = "c1aebba",
   --   pin = true,
@@ -142,60 +103,58 @@ local plugins = {
   -- },
   {
     'romgrk/barbar.nvim',
-    dependencies = {
-      'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
-      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    dependencies = { 'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons'            -- OPTIONAL: for file icons
     },
-    init = function() vim.g.barbar_auto_setup = false end,
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
     opts = {
       -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
       -- animation = true,
       -- insert_at_start = true,
       -- …etc.
     },
-    version = '^1.0.0', -- optional: only update when a new 1.x version is released
-  },
-  -- {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
+    version = '^1.0.0' -- optional: only update when a new 1.x version is released
+  },                   -- {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
+  -- ** Back on track **--
   {
-   "rmagatti/auto-session",
-   lazy = false,
-   enabled = false,
+    "rmagatti/auto-session",
+    lazy = false,
+    enabled = false,
 
-   ---enables autocomplete for opts
-   ---@module "auto-session"
-   ---@type AutoSession.Config
-   opts = {
-     suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-     -- log_level = 'debug',
-   },
-  },
-
-  --** Other **--
-  { 'skywind3000/vim-quickui' },
-  { 'TamaMcGlinn/quickfixdd' },
-  { 'farmergreg/vim-lastplace' },
-  {
-    'goolord/alpha-nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    pin = true
-  },
-  {
-    "christoomey/vim-tmux-navigator",
-    pin = true
-  },
-  {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    main = "ibl",
-    pin = true,
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
     opts = {
-      indent = { char = '┊' }
-    },
+      suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" }
+      -- log_level = 'debug',
+    }
+  }, { 'farmergreg/vim-lastplace' }, -- ** Other **--
+  { 'skywind3000/vim-quickui' }, { 'TamaMcGlinn/quickfixdd' }, {
+  'goolord/alpha-nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  pin = true
+}, {
+  "christoomey/vim-tmux-navigator",
+  pin = true
+}, {
+  -- Add indentation guides even on blank lines
+  'lukas-reineke/indent-blankline.nvim',
+  -- Enable `lukas-reineke/indent-blankline.nvim`
+  -- See `:help indent_blankline.txt`
+  main = "ibl",
+  pin = true,
+  opts = {
+    indent = {
+      char = '┊'
+    }
   },
   -- { 'HiPhish/rainbow-delimiters.nvim', pin = true },
-  { 'djoshea/vim-autoread',        pin = true },
+  {
+    'djoshea/vim-autoread',
+    pin = true
+  },
   {
     'nvim-lualine/lualine.nvim',
     pin = true,
@@ -205,20 +164,30 @@ local plugins = {
     }
   },
   -- These optional plugins should be loaded directly because of a bug in Packer lazy loading
-  { 'nvim-tree/nvim-web-devicons', pin = true }, -- OPTIONAL: for file icons
-  { 'lewis6991/gitsigns.nvim',     pin = true }, -- OPTIONAL: for git status
+  {
+    'nvim-tree/nvim-web-devicons',
+    pin = true
+  },   -- OPTIONAL: for file icons
+  {
+    'lewis6991/gitsigns.nvim',
+    pin = true
+  },   -- OPTIONAL: for git status
   {
     'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate',
     pin = true
   },
-  { 'tpope/vim-commentary', pin = true },
+  {
+    'tpope/vim-commentary',
+    pin = true
+  },
   {
     "iamcco/markdown-preview.nvim",
+    enabled = true,
     build = function()
       vim.fn["mkdp#util#install"]()
     end
-  },
-}
+  }
+} }
 
 require("lazy").setup(plugins, {})
