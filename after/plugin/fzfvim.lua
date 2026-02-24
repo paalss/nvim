7d6e14e6504ec9a3737e8158ed33913fded10e51
Pål Stakvik <pal.stakvik@visma.com>
2026-02-24 16:48:10 +0100

    feat(fzf): search word under cursor

--- after/plugin/fzfvim.lua
+++ after/plugin/fzfvim.lua
@@ -1,11 +1,13 @@
 -- https://github.com/junegunn/fzf?tab=readme-ov-file#vimneovim-plugin
 -- https://github.com/junegunn/fzf.vim
 -- https://github.com/junegunn/fzf/blob/master/README-VIM.md
-local builtin = require('telescope.builtin')
-local actions = require('telescope.actions')
-local telescope = require("telescope")
+-- local builtin = require('telescope.builtin')
+-- local actions = require('telescope.actions')
+-- local telescope = require("telescope")
 vim.keymap.set("n", "<C-p>", ":FZF<CR>", { desc = "Search files" })
 vim.keymap.set("n", "<leader>p0", ":RG<CR>", { desc = "Search text" })
+vim.keymap.set("n", "<leader>pø", ":RG <C-r><C-w><CR>", { desc = "Search text under cursor" })
+-- vim.keymap.set("n", "<leader>pa", ":Ag <C-R><C-W><CR>", { desc = "dsuifahfid" }) -- from https://github.com/junegunn/fzf.vim/issues/50
 -- vim.keymap.set("n", "<C-p>", ":FZF!<CR>", { desc = "Full screen" })
 -- vim.keymap.set("n", "<leader>p0", ":grep -R ", { desc = "Search files" })
 -- vim.keymap.set("n", "<C-p>", ":find **/", { desc = "Find files" })
