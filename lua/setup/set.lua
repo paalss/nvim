b7299496cc3c7b8a48df68441159b0abc76c54a2
Pål Stakvik <pal.stakvik@visma.com>
2026-03-11 11:20:39 +0100

    turn off swapfile

--- lua/setup/set.lua
+++ lua/setup/set.lua
@@ -23,7 +23,7 @@ vim.opt.smartindent = false
 vim.opt.wrap = false
 
 -- -- undotree
--- vim.opt.swapfile = false
+vim.opt.swapfile = false
 -- vim.opt.backup = false
 -- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 -- vim.opt.undofile = true
