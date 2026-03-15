37d61005e7b8a618078cb86a15f4c019cb5be9ca
paalss <paalss94@gmail.com>
2026-03-15 20:59:50 +0100

    chore(lazy-lock): update lockfile, add vim-surround comments

--- after/plugin/vim-surround.lua
+++ after/plugin/vim-surround.lua
@@ -17,6 +17,26 @@ autocmd FileType markdown let b:surround_{char2nr("f")} = "**\r**"
 
 
 
+-- Fork: tomtomjhj/vim-surround
+--    https://github.com/tomtomjhj/vim-surround/tree/delete-custom
+
+-- Open PR: Support deleting/changing custom surrounds
+--    tomtomjhj - 4. jun 2022
+--    https://github.com/tpope/vim-surround/pull/355
+--    fixes
+--       issue: Why are some matches a one way street? #199
+--          https://github.com/tpope/vim-surround/issues/199
+--       issue: deleting custom surrounds #210
+--          https://github.com/tpope/vim-surround/issues/210
+--       issue: Undo a custom surround? #318
+--          https://github.com/tpope/vim-surround/issues/318
+--       issue: How to apply custom mappings to ds, (ys works) #346
+--          https://github.com/tpope/vim-surround/issues/346
+
+-- Issue: Custom surroundings don't work with cs or ds #329
+--    hbarcelos - 14. jul 2021
+--    https://github.com/tpope/vim-surround/issues/329
+--    https://github.com/tpope/vim-surround/issues/329#issuecomment-922978875
 
 
 
