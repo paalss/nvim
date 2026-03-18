d2b74ef3380f7b49e5ed7d97cb466be4a4a07afb
Pål Stakvik <pal.stakvik@visma.com>
2026-03-18 16:58:52 +0100

    attempt at fzf-resume

---
+++ after/plugin/fzflua.lua
@@ -0,0 +1,7 @@
+-- not working??
+vim.keymap.set("n", "<leader>sr", ":FzfLua resume<CR>", { desc = "Resume previous search" })
+
+
+-- not necessary as this is covered by fzf.vim
+-- :FzfLua live_grep
+-- lua
