02adf52f0ede0f42b5b1e65db52c487d3ca652ac
Pål Stakvik <pal.stakvik@visma.com>
2026-08-19 14:47:43 +0200

    separate keymaps and do not continue to next [*]<leader>x automatically

--- lua/setup/remap.lua
+++ lua/setup/remap.lua
@@ -350,17 +350,23 @@ vim.keymap.set("n", "1<leader>x", "jV/====<CR>d", { desc = "remove unimportant d
 --   vim.cmd("d")
 -- end, { desc = "remove unimportant details" })
 
-vim.keymap.set("n", "8<leader>x",
+vim.keymap.set("n", "7<leader>x",
+  -- "ggVGp8<leader>x",
   "ggVGp",
-  { desc = "for full.txt: paste content over entire buffer" })
+  { remap = true, desc = "for full.txt: paste content over entire buffer" })
 
-vim.keymap.set("n", "88<leader>x",
-  "ggyG:e summary.txt<CR>ggVGpggjV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>d",
-  { desc = "for full.txt: Create summary.txt content based on open buffer" })
+vim.keymap.set("n", "8<leader>x",
+  "ggyG:e summary.txt<CR>ggVGp",
+  -- "ggyG:e summary.txt<CR>ggVGp9<leader>x",
+  { remap = true, desc = "for full.txt: Copy content of open buffer to summary.txt" })
 
 vim.keymap.set("n", "9<leader>x",
-  "ggjV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>d",
-  { desc = "for summary.txt: remove unimportant details" })
+  "ggO<esc>jV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>d",
+  { desc = "for summary.txt: turn the full-format to summary-format" })
+
+-- vim.keymap.set("n", "9<leader>x",
+--   "ggjV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>djV/====<CR>d",
+--   { desc = "for summary.txt: remove unimportant details" })
 
 -- TODO: make it work for only one specific buffer type
 -- TODO: test disse keymaps. Fungerer de i praksis?
